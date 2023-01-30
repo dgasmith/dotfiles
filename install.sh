5cee449ff101710467f58770e47b765ba8024636
Daniel Smith <dsmith@fl83inc.com>
2023-01-30 15:08:54 +0000

    Do not change shell

--- install.sh
+++ install.sh
@@ -4,7 +4,6 @@
 if [ ! -d ~/.oh-my-zsh/ ]; then
   sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)";
 fi
-chsh -s /usr/bin/zsh $USERNAME
 
 cp .zshrc ~/.zshrc
 cp .vimrc ~/.vimrc
