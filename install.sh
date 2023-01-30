c1519a321fa230c7cd4d9c729c65db4043500692
Daniel Smith <dsmith@fl83inc.com>
2023-01-30 14:15:04 +0000

    Root shell

--- install.sh
+++ install.sh
@@ -2,3 +2,7 @@
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 chsh -s /usr/bin/zsh $USERNAME
+
+cp .zshrc ~/.zshrc
+cp .vimrc ~/.vimrc
+cp -r .vim ~/.vim
